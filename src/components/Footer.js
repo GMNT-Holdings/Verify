b06292852cabae23c6165afcb197d98118fbbca5
tsakaniFlorence <tsakane@gmntholding.co.za>
2024-10-29 10:07:17 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ function Footer() {
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
           <div>
             <h4 className="text-lg font-semibold mb-4">Contact Us</h4>
-            <p className="mb-2 text-gray-400"><strong className="text-white">Phone:</strong> +27 123 456 789</p>
+            <p className="mb-2 text-gray-400"><strong className="text-white">Phone:</strong>082 746 7928</p>
             <p className="mb-2 text-gray-400"><strong className="text-white">Email:</strong> <a href="mailto: info@verifynow.co.za" className="hover:text-blue-400 transition duration-300"> info@verifynow.co.za</a></p>
             <p className="mb-2 text-gray-400"><strong className="text-white">Address:</strong> Thornhill Office Park, Vorna Valley , Midrand, Johannesburg, 1685</p>
           </div>
